4403166e6157301711c50d70a216451c5b5889fb
m-zayan <zayanm410@gmail.com>
2019-12-02 22:48:33 +0200

    Sign In UI

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Omail.Components;
+using Omail.API.API_Validator;
+namespace Omail
+{
+
+    public partial class MainWindow :Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public event PropertyChangedEventHandler propertyChanged;
+
+        
+        public void OnPropertyChanged([CallerMemberName] string propertyName=null)
+        {
+            propertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        
+        private void Login(object sender, RoutedEventArgs evnt)
+        {
+
+
+            string username = Username.Value;
+            string password = Password.Value;
+
+            
+            MessageBox.Show(password);
+           
+
+
+
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,18 @@
+# __Credit System UI Images__
+&#9673; Logo Icon made by [kiranshastry](https://www.flaticon.com/authors/kiranshastry) from [flaticon](www.flaticon.com)<br>
+&#9673; Seo-And-Business & UseInterface Icons made by [srip](https://www.flaticon.com/authors/srip) from [flaticon](www.flaticon.com)
+_________
+# __About__
+<img src="Images-Omail/Logo.png" alt="Icon" width="250" height = "250"/> <h1>Omail</h1>
+
+**Omail System** is a Windows app made to facilitate communication needs at Organizations & Startups.
+
+### Authors &#9997;	
+__________________
+<pre>
+  &#9673; Mohamed Osama Mohamed Zayan
+  &#9673; Mohammed Ali Abdullah El - Feky  	 
+  &#9673; Omar Hafez Abdullah Hafez  
+  &#9673; Moataz Gamal Abdel Gawad El Sayed  
+  &#9673; Mahmoud Mohamed Hosny Mostafa
+</pre>
