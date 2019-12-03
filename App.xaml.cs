f557a50ba12dcb5bc6112d21a6834c226a21e440
m-zayan <zayanm410@gmail.com>
2019-12-03 19:26:48 +0200

    UI Navigation

---
+++ App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Omail
+{
+    public partial class App : Application
+    {
+        private void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs Error)
+        {
+            MessageBox.Show("An unhandled exception just occurred: " + Error.Exception.Message, "Exception", MessageBoxButton.OK, MessageBoxImage.Warning);
+            Error.Handled = true;
+        }
+    }
+}
