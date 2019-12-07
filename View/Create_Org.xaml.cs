03479a4d9d777c159d4d94ece240354a2e573dc5
m-zayan <zayanm410@gmail.com>
2019-12-07 05:30:20 +0200

    UI Add Org

--- View/Create_Org.xaml.cs
+++ View/Create_Org.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
+
 
 namespace Omail.View
 {
@@ -23,6 +25,49 @@ namespace Omail.View
         public Create_Org()
         {
             InitializeComponent();
+        }
+
+        private void Load_Image(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            op.Title = "Select a picture";
+            op.Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" +
+              "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
+              "Portable Network Graphic (*.png)|*.png";
+            if (op.ShowDialog() == true)
+            {
+                ImageFile.Text = op.FileName;
+                Image.Source = new BitmapImage(new Uri(op.FileName));
+            }
+        }
+
+        private void SkipLogo_Checked(object sender, RoutedEventArgs e)
+        {
+            ImageButton.IsEnabled = false;
+        }
+
+        private void SkipLogo_Unchecked(object sender, RoutedEventArgs e)
+        {
+            if(ImageButton.IsEnabled == false)
+            {
+                ImageButton.IsEnabled = true;
+            }
+        }
+
+        private void SkipStructureChecked(object sender, RoutedEventArgs e)
+        {
+            OrgType.IsEnabled = false;
+            OM.IsEnabled = false;
+            GM.IsEnabled = false;
+            Emp.IsEnabled = false;
+        }
+
+        private void SkipStructureUnchecked(object sender, RoutedEventArgs e)
+        {
+            OrgType.IsEnabled = true;
+            OM.IsEnabled = true;
+            GM.IsEnabled = true;
+            Emp.IsEnabled = true;
         }
     }
 }
